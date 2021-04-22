b33f02e645b2aada067869358fb6bd92e03f02ad
joshua_meleski@taylor.edu <jmeleski@cse21811.cs.local>
2021-04-21 21:53:49 -0400

    Milestone 2: Server.c

--- server.c/main.c
+++ server.c/main.c
@@ -3,32 +3,34 @@
 #include <netdb.h>
 #include <string.h>
 #include <errno.h>
-#include <sys/stat.h>
-#include "unistd.h"
-#include "stdlib.h"
-#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 
-#define SERVER "10.121.218.11"
-#define SPORT "8080"
+
+// #define SERVER "10.121.218.11"
+#define SPORT "4950"
+
 // #define CLIENT "10.121.218.10"
-#define MAX_CLIENT_BACKLOG 128
+// #define MAX_CLIENT_BACKLOG 128
 #define MAX_BUFFER_SIZE 4096
-#define IS_MULTIPROCESS 0
+// #define IS_MULTIPROCESS 0
 
 int main(int argc, char* argv[]) {
 
 
-    int socket_desc, accept_desc;
     int return_value;
-    int enable = 1;
-
+    int num_bytes;
     char *HOST = argv[1];
     char *CLIENT = argv[2];
     char *PORT = argv[3];
-    struct addrinfo hints;
+    struct addrinfo hints, *p;
     struct addrinfo * address_resource;
     struct sockaddr_storage remote_addr;
+    char buf[MAX_BUFFER_SIZE];
     socklen_t remote_addr_s = sizeof(remote_addr);
+   // char s[INET_ADDRSTRLEN];
 
     printf("%s \n", HOST);
     printf("%s \n", PORT);
@@ -41,73 +43,44 @@ int main(int argc, char* argv[]) {
     // SOCK_DGRAM: possibly unordered packets (e.g. UDP)
     hints.ai_flags = AI_PASSIVE; // Listen
 
-    return_value = getaddrinfo(SERVER, SPORT, &hints, &address_resource);
+    return_value = getaddrinfo(NULL, SPORT, &hints, &address_resource);
     if (return_value != 0) {
         printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
-        return return_value;
     }
 
-    // socket(): creates a new TCP/IP socket, no address or port is assigned yet
-    socket_desc = socket(
-            address_resource->ai_family,    // IPv4
-            address_resource->ai_socktype,  // Streaming Protocol
-            address_resource->ai_protocol   // TCP
-    );
-    if (socket_desc == -1) {
-        printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
-        return socket_desc;
+    // socket(): creates a new UDP socket, no address or port is assigned yet
+    int socket_desc;
+    // Loops until client is found
+    for  (p = address_resource; p != NULL ; p = p->ai_next) {
+        if( (socket_desc = socket(p->ai_family, p->ai_socktype, p->ai_protocol))  == -1) {
+            printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
+            continue;
+        }
+        if (bind(socket_desc, p->ai_addr, p->ai_addrlen ) == -1){
+            close(socket_desc);
+            printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
+            continue;
+        }
+        break;
     }
 
-    return_value = setsockopt(socket_desc, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
-    if (return_value == -1) {
+    if(p == NULL){
         printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
-        return return_value;
-    }
-
-    // bind(): assign address and port to socket
-    return_value = bind(socket_desc, address_resource->ai_addr, address_resource->ai_addrlen);
-    if (return_value == -1) {
-        printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
-        return return_value;
     }
 
     freeaddrinfo(address_resource);
 
-    return_value = listen(socket_desc, MAX_CLIENT_BACKLOG);
-    if (return_value == -1) {
-        printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
-        return return_value;
-    }
+    printf("Listener: waiting to recvfrom... \n");
 
-/*
-    while(1){
+    remote_addr_s = sizeof(remote_addr);
+    num_bytes = recvfrom(socket_desc, buf, MAX_BUFFER_SIZE-1, 0, (struct sockaddr *) &remote_addr, &remote_addr_s);
 
+    if (num_bytes == -1)
+        printf("Error: recvfrom %s (line: %d)\n", strerror(errno), __LINE__);
 
-        accept_desc = accept(socket_desc, (struct sockaddr *) &remote_addr, &remote_addr_s);
-
-#if IS_MULTIPROCESS == 1
-        int pid = fork();
-            if (pid == 0){
-                //...child process starts
-                handle_connection(accept_desc, DOCROOT);
-                close(accept_desc);
-                close(socket_desc);
-                exit(0);
-                //...child process ends
-            }
-            if (pid == -1){
-                printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
-                return pid;
-            }
-
-        close(accept_desc);
-
-#else
-        handle_connection(accept_desc, DOCROOT);
-        close(accept_desc);
-#endif
-    }
-*/
+    printf("Received packet \nPacket is %d long\n", num_bytes);
+    buf[num_bytes] = '\0';
+    printf("PACKET CONTENTS: %s ", buf);
 
     close(socket_desc);
 
