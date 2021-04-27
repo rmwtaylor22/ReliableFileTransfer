25aa45d8d45a21b91c1a56e99b185eff87be9d15
joshua_meleski@taylor.edu <jmeleski@cse21809.cs.local>
2021-04-26 23:19:35 -0400

    Server can communicate back to Client

--- server.c/main.c
+++ server.c/main.c
@@ -5,26 +5,71 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
+//#include <sys/types.h>
+//#include <arpa/inet.h>
 
 
 // #define SERVER "10.121.218.11"
-#define SPORT "4950"
 
-// #define CLIENT "10.121.218.10"
+
+#define CLIENT "10.121.218.10"
 // #define MAX_CLIENT_BACKLOG 128
 #define MAX_BUFFER_SIZE 4096
-// #define IS_MULTIPROCESS 0
+
+int socket_desc;
+void send_it(struct addrinfo *p){
+    char *buf;
+    buf = "HELLO THERE";
+    int numbytes;
+    if ((numbytes = sendto (socket_desc, buf, strlen(buf), 0, p->ai_addr, p->ai_addrlen)) == -1){
+        perror("talker: sendto");
+        exit(1);
+    }
+    printf("talker: sent %d bytes to %s\n", numbytes, CLIENT);
+}
+
+void process_content(socklen_t remote_addr_s,struct sockaddr_storage remote_addr) {
+    int num_bytes;
+    char buf[MAX_BUFFER_SIZE];
+    remote_addr_s = sizeof(remote_addr);
+    num_bytes = recvfrom(socket_desc, buf, MAX_BUFFER_SIZE - 1, 0, (struct sockaddr *) &remote_addr, &remote_addr_s);
+
+    if (num_bytes == -1)
+        printf("Error: recvfrom %s (line: %d)\n", strerror(errno), __LINE__);
+
+    printf("Received packet \nPacket is %d long\n", num_bytes);
+    buf[num_bytes] = '\0';
+    printf("PACKET CONTENTS: %s \n", buf);
+
+}
+void Listen(struct addrinfo *address_resource, struct addrinfo *p, socklen_t remote_addr_s,struct sockaddr_storage remote_addr){
+    // socket(): creates a new UDP socket, no address or port is assigned yet
+
+    // Loops until client is found
+    for  (p = address_resource; p != NULL ; p = p->ai_next) {
+        if( (socket_desc = socket(p->ai_family, p->ai_socktype, p->ai_protocol))  == -1) {
+            printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
+            return;
+        }
+        if (bind(socket_desc, p->ai_addr, p->ai_addrlen ) == -1){
+            close(socket_desc);
+            printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
+            return;
+        }
+        break;
+    }
+    if(p == NULL){
+        printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
+    }
+
+    printf("Listener: waiting to recv from... \n");
+    process_content(remote_addr_s, remote_addr);
+}
 
 int main(int argc, char* argv[]) {
 
-
-    int return_value;
-    int num_bytes;
-    char *HOST = argv[1];
-    char *CLIENT = argv[2];
-    char *PORT = argv[3];
+    int return_value, rv;
+    char *PORT = argv[1];
     struct addrinfo hints, *p;
     struct addrinfo * address_resource;
     struct sockaddr_storage remote_addr;
@@ -32,7 +77,6 @@ int main(int argc, char* argv[]) {
     socklen_t remote_addr_s = sizeof(remote_addr);
    // char s[INET_ADDRSTRLEN];
 
-    printf("%s \n", HOST);
     printf("%s \n", PORT);
 
 
@@ -43,45 +87,33 @@ int main(int argc, char* argv[]) {
     // SOCK_DGRAM: possibly unordered packets (e.g. UDP)
     hints.ai_flags = AI_PASSIVE; // Listen
 
-    return_value = getaddrinfo(NULL, SPORT, &hints, &address_resource);
+    return_value = getaddrinfo(NULL, PORT, &hints, &address_resource);
     if (return_value != 0) {
         printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
     }
 
-    // socket(): creates a new UDP socket, no address or port is assigned yet
-    int socket_desc;
-    // Loops until client is found
-    for  (p = address_resource; p != NULL ; p = p->ai_next) {
-        if( (socket_desc = socket(p->ai_family, p->ai_socktype, p->ai_protocol))  == -1) {
-            printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
-            continue;
-        }
-        if (bind(socket_desc, p->ai_addr, p->ai_addrlen ) == -1){
-            close(socket_desc);
-            printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
+
+
+    Listen(address_resource, p, remote_addr_s, remote_addr);
+    freeaddrinfo(address_resource);
+
+    if((rv = getaddrinfo(CLIENT, PORT, &hints, &address_resource)) !=0){
+        fprintf(stderr,"getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+    for(p = address_resource; p != NULL; p = p->ai_next) {
+        if((socket_desc = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("talker: socket");
             continue;
         }
         break;
     }
-
-    if(p == NULL){
-        printf("Error: %s (line: %d)\n", strerror(errno), __LINE__);
+    if (p == NULL) {
+        fprintf(stderr, "talker: failed to create socket\n");
+        return 2;
     }
 
-    freeaddrinfo(address_resource);
-
-    printf("Listener: waiting to recvfrom... \n");
-
-    remote_addr_s = sizeof(remote_addr);
-    num_bytes = recvfrom(socket_desc, buf, MAX_BUFFER_SIZE-1, 0, (struct sockaddr *) &remote_addr, &remote_addr_s);
-
-    if (num_bytes == -1)
-        printf("Error: recvfrom %s (line: %d)\n", strerror(errno), __LINE__);
-
-    printf("Received packet \nPacket is %d long\n", num_bytes);
-    buf[num_bytes] = '\0';
-    printf("PACKET CONTENTS: %s ", buf);
-
+    send_it(p);
     close(socket_desc);
 
     return 0;
